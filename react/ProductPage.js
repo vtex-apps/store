639770bcd3b9e48e81f28115f926d457744dd504
Bruno Dias <bruno.dias@vtex.com.br>
2018-06-04 18:39:51 -0300

    Remove redundant Spinner in ProductPage Component

--- react/ProductPage.js
+++ react/ProductPage.js
@@ -2,45 +2,40 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'react-apollo'
 import { ExtensionPoint } from 'render'
-
 import productQuery from './queries/productQuery.gql'
 
-import WrappedSpinner from './components/WrappedSpinner'
-
 class ProductPage extends Component {
-  static contextTypes = {
-    prefetchPage: PropTypes.func,
-  }
-
   static propTypes = {
     params: PropTypes.object,
     data: PropTypes.object,
+    prefetch: PropTypes.func,
   }
+
   state = {
     isModalOpen: false,
   }
 
   componentDidMount() {
-    this.context.prefetchPage('store/home')
+    this.props.prefetch('store/home')
   }
 
   render() {
     const { data } = this.props
     const { loading, variables, product } = data
+
     return (
       <div>
-        {loading ? (
-          <WrappedSpinner />
-        ) : (
-            <div>
-              <div className="vtex-product-details-container">
-                <ExtensionPoint
-                  id="container"
-                  slug={variables.slug}
-                  categories={product.categories} />
-              </div>
-            </div>
-          )}
+        <div className="pv9-ns">
+          <div className="vtex-product-details-container">
+            {!loading && (
+              <ExtensionPoint
+                id="sections"
+                slug={variables.slug}
+                categories={product.categories}
+              />
+            )}
+          </div>
+        </div>
       </div>
     )
   }
