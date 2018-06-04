5bf24128a190932e7072db15c515d419d96a0933
Bruno Abreu <bruno.abreu@vtex.com.br>
2018-06-04 13:35:51 -0300

    Remove global css

--- react/StoreTemplate.js
+++ react/StoreTemplate.js
@@ -2,8 +2,6 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { ExtensionPoint } from 'render'
 
-import './store.global.css'
-
 export default class StoreTemplate extends Component {
   static propTypes = {
     children: PropTypes.element,
@@ -12,6 +10,7 @@ export default class StoreTemplate extends Component {
   render() {
     return (
       <Fragment>
+        <ExtensionPoint id="theme" />
         <ExtensionPoint id="header" />
         <div className="vtex-store__template w-100 h-100">{this.props.children}</div>
         <ExtensionPoint id="footer" />
