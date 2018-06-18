36756c029f364cbf6a6a7ffdfd341f7676a6ac4e
Claudivan Filho <j.claudivan.filho@gmail.com>
2018-06-18 10:39:39 -0300

    Fix SearchPage ExtensionPoint props (#6)
    * Fix SearchPage extension point props
    
    * Fix store/search path

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+- Pass props to the extensionPoint of `SearchPage`.
+- Modify `store/search` path from `/:term/` to `/:term/s`
 
 ## [0.2.5] - 2018-6-14
 
--- react/SearchPage.js
+++ react/SearchPage.js
@@ -5,13 +5,27 @@ import { ExtensionPoint } from 'render'
 export default class SearchPage extends Component {
   static propTypes = {
     params: PropTypes.shape({
+      /** Search's term, e.g: eletronics. */
       term: PropTypes.string.isRequired,
+    }),
+    query: PropTypes.shape({
+      /**
+       * Rest of the search term, e.g: eletronics/smartphones/samsung implies that
+       * rest will be equal to "smartphones,samsung".
+       * */
+      rest: PropTypes.string,
+      /** Determines the types of the terms, e.g: "c,c,b" (category, category, brand). */
+      map: PropTypes.string,
+      /** Search's pagination.  */
+      page: PropTypes.string,
+      /** Search's ordenation. */
+      order: PropTypes.string,
     }),
   }
 
   render() {
     return (
-      <ExtensionPoint id="container" />
+      <ExtensionPoint id="container" {...this.props} />
     )
   }
 }
