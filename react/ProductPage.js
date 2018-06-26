cf5491375cff6cebc021e215caf59405a103394d
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-06-26 11:11:07 -0300

    Add initial dataLayer components

--- react/ProductPage.js
+++ react/ProductPage.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { graphql } from 'react-apollo'
+import { graphql, compose } from 'react-apollo'
 import { ExtensionPoint } from 'render'
+
+import withDataLayer, { dataLayerProps } from './components/withDataLayer'
 import productQuery from './queries/productQuery.gql'
 
 class ProductPage extends Component {
   static contextTypes = {
     prefetchPage: PropTypes.func,
   }
+
   static propTypes = {
     params: PropTypes.object,
     data: PropTypes.object,
+    ...dataLayerProps,
+  }
+
+  pushToDataLayer = product => {
+    this.props.pushToDataLayer({
+      ecommerce: {
+        detail: {
+          products: [{
+            name: product.productName,
+            brand: product.brand,
+          }],
+        },
+      },
+    })
   }
 
   componentDidMount() {
     this.context.prefetchPage('store/home')
+
+    if (!this.props.data.loading) {
+      this.pushToDataLayer(this.props.data.product)
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.data.loading && !this.props.data.loading) {
+      this.pushToDataLayer(this.props.data.product)
+    }
   }
 
   render() {
@@ -22,17 +49,15 @@ class ProductPage extends Component {
     const { loading, variables, product } = data
 
     return (
-      <div>
-        <div className="vtex-product-details-container">
-          {!loading && (
-            <ExtensionPoint
-              id="container"
-              slug={variables.slug}
-              categories={product.categories}
-              product={product}
-            />
-          )}
-        </div>
+      <div className="vtex-product-details-container">
+        {!loading && (
+          <ExtensionPoint
+            id="container"
+            slug={variables.slug}
+            categories={product.categories}
+            product={product}
+          />
+        )}
       </div>
     )
   }
@@ -46,6 +71,7 @@ const options = {
   }),
 }
 
-export default graphql(productQuery, options)(
-  ProductPage
-)
+export default compose(
+  graphql(productQuery, options),
+  withDataLayer
+)(ProductPage)
