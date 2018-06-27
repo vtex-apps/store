d6a2399d8586591d37c6bdb88cc64a9d07a0b42c
Estácio Pereira <estacio.pereira@gmail.com>
2018-06-27 12:18:01 -0300

    Initial GraphQL addition in SearchPage

--- react/SearchPage.js
+++ react/SearchPage.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import { compose, graphql } from 'react-apollo'
 import { ExtensionPoint } from 'render'
 
-export default class SearchPage extends Component {
+import { facetsQueryShape, searchQueryShape } from './constants/propTypes'
+import { createMap, joinPathWithRest, reversePagesPath } from './helpers/searchHelpers'
+import facetsQuery from './queries/facetsQuery.gql'
+import searchQuery from './queries/searchQuery.gql'
+
+const DEFAULT_PAGE = 1
+
+class SearchPage extends Component {
   static propTypes = {
     params: PropTypes.shape({
       /** Search's term, e.g: eletronics. */
       term: PropTypes.string.isRequired,
+      /** Department param. */
+      department: PropTypes.string,
+      /** Category param. */
+      category: PropTypes.string,
+      /** Subcategory param. */
+      subcategory: PropTypes.string,
     }),
     query: PropTypes.shape({
       /**
@@ -21,11 +35,68 @@ export default class SearchPage extends Component {
       /** Search's ordenation. */
       order: PropTypes.string,
     }),
+    /** Internal route path. e.g: 'store/search' */
+    treePath: PropTypes.string,
+    /** Facets graphql query. */
+    facetsQuery: facetsQueryShape,
+    /** Search graphql query. */
+    searchQuery: searchQueryShape,
   }
 
   render() {
-    return (
-      <ExtensionPoint id="container" {...this.props} pagesPath={this.props.treePath}/>
-    )
+    const {
+      treePath,
+      query: { order: orderBy, page: pageProps, map: mapProps, rest },
+    } = this.props
+    const pathName = reversePagesPath(treePath, this.props.params)
+    const map = mapProps || createMap(pathName, rest)
+    const page = pageProps ? parseInt(pageProps) : DEFAULT_PAGE
+    const containerProps = {
+      ...this.props,
+      path: pathName,
+      map,
+      rest,
+      page,
+      orderBy,
+      pagesPath: treePath,
+    }
+    return <ExtensionPoint id="container" {...containerProps} />
   }
 }
+
+export default compose(
+  graphql(facetsQuery, {
+    name: 'facetsQuery',
+    options: props => {
+      const { treePath, params, rest, map: mapProps } = props
+      const path = reversePagesPath(treePath, params)
+      const query = joinPathWithRest(path, rest)
+      const facets = `${query}?map=${mapProps || createMap(path, rest)}`
+      return {
+        variables: { facets },
+      }
+    },
+  }),
+  graphql(searchQuery, {
+    name: 'searchQuery',
+    options: props => {
+      const {
+        treePath,
+        rest,
+        map,
+        params,
+        orderBy,
+        page,
+        maxItemsPerPage,
+      } = props
+      const path = reversePagesPath(treePath, params)
+      const query = joinPathWithRest(path, rest)
+      const from = (page - 1) * maxItemsPerPage
+      const to = from + maxItemsPerPage - 1
+      return {
+        variables: { query, map, orderBy, from, to },
+        notifyOnNetworkStatusChange: true,
+      }
+    },
+  })
+)(SearchPage)
---
+++ react/helpers/searchHelpers.js
@@ -0,0 +1,124 @@
+import QueryString from 'query-string'
+import * as RouteParser from 'route-parser'
+
+import SortOptions from '../constants/searchSortOptions'
+
+export function joinPathWithRest(path, rest) {
+  let pathValues = stripPath(path).split('/')
+  pathValues = pathValues.concat((rest && rest.split(',')) || [])
+  return pathValues.join('/')
+}
+
+export function getCategoriesFromQuery(query, map) {
+  return getValuesByMap(query, map, 'c')
+}
+
+function getValuesByMap(query, map, mapValue) {
+  const values = query.split('/')
+  const mapValues = map.split(',')
+  const brands = []
+  mapValues.map((value, index) => {
+    if (value === mapValue) {
+      brands.push(values[index])
+    }
+  })
+  return brands
+}
+
+export function findInTree(tree, values, index) {
+  for (let i = 0; i < tree.length; i++) {
+    if (tree[i].Name.toUpperCase() === values[index].toUpperCase()) {
+      if (index === values.length - 1) {
+        return tree[i]
+      }
+      return findInTree(tree[i].Children, values, index + 1)
+    }
+  }
+  return tree[0]
+}
+
+export function createMap(pathName, rest, isBrand) {
+  let pathValues = stripPath(pathName).split('/')
+  if (rest) pathValues = pathValues.concat(rest.split(','))
+  const map =
+    Array(pathValues.length - 1)
+      .fill('c')
+      .join(',') +
+    (pathValues.length > 1 ? ',' : '') +
+    (isBrand ? 'b' : 'c')
+  return map
+}
+
+export function stripPath(pathName) {
+  return pathName
+    .replace(/^\//i, '')
+    .replace(/\/s$/i, '')
+    .replace(/\/d$/i, '')
+    .replace(/\/b$/i, '')
+}
+
+function getPathOfPage(pagesPath) {
+  return global.__RUNTIME__.pages[pagesPath].path
+}
+
+export function reversePagesPath(pagesPath, params) {
+  const Parser = RouteParser.default ? RouteParser.default : RouteParser
+  return new Parser(getPathOfPage(pagesPath)).reverse(params)
+}
+
+function matchPagesPath(pagesPath, pathName) {
+  const Parser = RouteParser.default ? RouteParser.default : RouteParser
+  return new Parser(pagesPath).match(pathName)
+}
+
+function getSpecificationFilterFromLink(link) {
+  return `specificationFilter_${link.split('specificationFilter_')[1]}`
+}
+
+export function getPagesArgs(
+  { name, type, link },
+  pathName,
+  rest,
+  { map, orderBy, pageNumber = 1 },
+  pagesPath,
+  isUnselectLink
+) {
+  const restValues = (rest && rest.split(',')) || []
+  const mapValues = (map && map.split(',')) || []
+  if (name) {
+    if (isUnselectLink) {
+      const pathValuesLength = stripPath(pathName).split('/').length
+      const index = restValues.findIndex(
+        item => name.toLowerCase() === item.toLowerCase()
+      )
+      if (index !== -1) {
+        restValues.splice(index, 1)
+        mapValues.splice(pathValuesLength + index, 1)
+      }
+    } else {
+      switch (type) {
+        case 'Brands': {
+          mapValues.push('b')
+          break
+        }
+        case 'SpecificationFilters': {
+          mapValues.push(`${getSpecificationFilterFromLink(link)}`)
+          break
+        }
+        default: {
+          mapValues.push('c')
+        }
+      }
+      restValues.push(name)
+    }
+  }
+
+  const queryString = QueryString.stringify({
+    map: mapValues.join(','),
+    page: pageNumber !== 1 ? pageNumber : undefined,
+    order: orderBy === SortOptions[0].value ? undefined : orderBy,
+    rest: restValues.join(',') || undefined,
+  })
+  const params = matchPagesPath(getPathOfPage(pagesPath), pathName)
+  return { page: pagesPath, params, queryString }
+}
