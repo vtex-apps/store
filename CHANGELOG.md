5ef96d1deb71b6de47a49e5135d03105f9e00675
Marcelo Tokarnia <marcelo.tokarnia@vtex.com.br>
2018-07-03 11:21:52 -0300

    Add Changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,24 +7,28 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [0.3.1] - 2018-7-03
+### Added
+- Add `MicroData` component do `ProductPage`, so Google can have a detailed info on the Products.
+
 ## [0.3.0] - 2018-6-25
 ### Added
 - Add `pagesPath` prop to the container of `SearchPage` and `CategoryPage`.
 
 ### Fixed
-- Add `Fragment` in `AccountPage`. 
+- Add `Fragment` in `AccountPage`.
 
 ## [0.2.9] - 2018-6-25
 ### Fixed
-- Add route `account/orders` again. 
+- Add route `account/orders` again.
 
 ## [0.2.8] - 2018-6-20
 
-### Changed 
+### Changed
 - Add `categoriesIds` on productQuery.
 
 ## [0.2.7] - 2018-6-20
-### Changed 
+### Changed
 - Add `variations` and `properties` on productQuery.
 
 ## [0.2.6] - 2018-6-18
@@ -37,9 +41,9 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ### Changed
 - Update AccountPage to use ExtensionContainer
 
-### Removed 
-- Remove `my-orders-app`. This app will be included in `dreamstore-theme`. 
-- Remove route `account/orders`. 
+### Removed
+- Remove `my-orders-app`. This app will be included in `dreamstore-theme`.
+- Remove route `account/orders`.
 
 
 ## [0.2.4] - 2018-6-14
--- react/components/MicroData.js
+++ react/components/MicroData.js
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { pathOr, path, map, sort, compose, head } from 'ramda'
 
+const ITEM_AVAILABLE = 100
+
 const lowestPriceInStockSeller = item => {
   if (item.sellers.length) {
-    return sort((a, b) => a.commertialOffer && a.commertialOffer.availableQuantity > 0
-      ? b.commertialOffer && b.commertialOffer.availableQuantity > 0
-        ? a.commertialOffer.price - b.commertialOffer.price
+    return sort((itemA, itemB) => itemA.commertialOffer && itemA.commertialOffer.availableQuantity > 0
+      ? itemB.commertialOffer && itemB.commertialOffer.availableQuantity > 0
+        ? itemA.commertialOffer.price - itemB.commertialOffer.price
         : -1
       : -1,
     item.sellers)[0]
@@ -16,7 +18,7 @@ const lowestPriceInStockSeller = item => {
 
 const lowestPriceItem = compose(
   head,
-  sort((a, b) => (path(['seller', 'commertialOffer', 'price'], a) - path(['seller', 'commertialOffer', 'price'], b)))
+  sort((itemA, itemB) => (path(['seller', 'commertialOffer', 'price'], itemA) - path(['seller', 'commertialOffer', 'price'], itemB)))
 )
 
 const lowestPriceInStock = product => {
@@ -37,7 +39,7 @@ const lowestPriceInStock = product => {
 }
 
 export default function MicroData({ product }, { culture: { currency } }) {
-  const { item, image, seller } = lowestPriceInStock(product)
+  const { image, seller } = lowestPriceInStock(product)
   return (
     <div vocab="http://schema.org/" typeof="Product">
       <span property="brand">{product.brand}</span>
@@ -56,7 +58,7 @@ export default function MicroData({ product }, { culture: { currency } }) {
           <span property="name">{seller.sellerName}</span>
         </span>
         Condition: <link property="itemCondition" href="http://schema.org/NewCondition" />New
-        { pathOr(100, ['commertialOffer', 'availableQuantity'], seller)
+        { pathOr(ITEM_AVAILABLE, ['commertialOffer', 'availableQuantity'], seller)
           ? <Fragment><link property="availability" href="http://schema.org/InStock"></link> In stock. Order now.</Fragment>
           : <Fragment><link property="availability" href="http://schema.org/OutOfStock"></link>Out of Stock</Fragment>
         }
