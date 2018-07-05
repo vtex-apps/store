b60e083d15cc37c2b28fd9e3c11f61bdb0e2ecee
Bruno Abreu <bruno.abreu@vtex.com.br>
2018-07-05 14:40:04 -0300

    Create new template structure

--- react/ProductSearchTemplate.js
+++ react/ProductSearchTemplate.js
@@ -6,16 +6,25 @@ import SearchQueryContainer from './components/SearchQueryContainer'
 import { searchQueryPropTypes } from './constants/propTypes'
 import { SearchQueryContext } from './constants/searchContext'
 
-export default class CategoryPage extends Component {
+export default class SearchPage extends Component {
   static contextTypes = {
     prefetchPage: PropTypes.func,
   }
 
   static propTypes = {
     params: PropTypes.shape({
-      department: PropTypes.string.isRequired,
+      /** Brand name */
+      brand: PropTypes.string,
+
+      /** handles /:department/d
+       *  or /:department/:category
+       *  or /:department/:category/:subcategory */
+      department: PropTypes.string,
       category: PropTypes.string,
       subcategory: PropTypes.string,
+
+      /** Search's term, e.g: eletronics. */
+      term: PropTypes.string,
     }),
     ...searchQueryPropTypes,
   }
@@ -25,8 +34,19 @@ export default class CategoryPage extends Component {
   }
 
   render() {
+    const props = {
+      ...this.props,
+      // todo: this logic should be in SearchQueryContainer
+      ...this.props.params.brand && {
+        query: {
+          ...this.props.query,
+          map: this.props.map || 'b',
+        }
+      }
+    }
+
     return (
-      <SearchQueryContainer {...this.props}>
+      <SearchQueryContainer {...props}>
         <SearchQueryContext.Consumer>
           {contextProps => <ExtensionPoint id="container" {...contextProps} />}
         </SearchQueryContext.Consumer>
