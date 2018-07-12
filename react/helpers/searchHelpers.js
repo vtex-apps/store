fc28f6c0b8858359be969ac3504d834c39d29ae1
Estácio Pereira <estacio.pereira@gmail.com>
2018-07-12 10:26:03 -0300

    Fix pages params evaluation. (#28)
    * Fix usage of the runtime elements
    
    * Fix pages params evaluation.
    
    * Delete runtime proptype
    
    * Import store pages file to better solve the problem
    
    * import pages.json at the begining of the file
    
    * delete one line function

--- react/helpers/searchHelpers.js
+++ react/helpers/searchHelpers.js
@@ -1,7 +1,6 @@
-import QueryString from 'query-string'
 import * as RouteParser from 'route-parser'
 
-import SortOptions from '../constants/searchSortOptions'
+import pagesJson from '../../pages/pages.json'
 
 export function joinPathWithRest(path, rest) {
   let pathValues = stripPath(path).split('/')
@@ -57,71 +56,7 @@ export function stripPath(pathName) {
     .replace(/\/b$/i, '')
 }
 
-function getPathOfPage(pagesPath) {
-  return (
-    global.__RUNTIME__.pages[pagesPath] &&
-    global.__RUNTIME__.pages[pagesPath].path
-  )
-}
-
 export function reversePagesPath(pagesPath, params) {
   const Parser = RouteParser.default ? RouteParser.default : RouteParser
-  return new Parser(getPathOfPage(pagesPath) || '').reverse(params)
-}
-
-function matchPagesPath(pagesPath, pathName) {
-  const Parser = RouteParser.default ? RouteParser.default : RouteParser
-  return new Parser(pagesPath).match(pathName)
-}
-
-function getSpecificationFilterFromLink(link) {
-  return `specificationFilter_${link.split('specificationFilter_')[1]}`
-}
-
-export function getPagesArgs(
-  { name, type, link },
-  pathName,
-  rest,
-  { map, orderBy, pageNumber = 1 },
-  pagesPath,
-  isUnselectLink
-) {
-  const restValues = (rest && rest.split(',')) || []
-  const mapValues = (map && map.split(',')) || []
-  if (name) {
-    if (isUnselectLink) {
-      const pathValuesLength = stripPath(pathName).split('/').length
-      const index = restValues.findIndex(
-        item => name.toLowerCase() === item.toLowerCase()
-      )
-      if (index !== -1) {
-        restValues.splice(index, 1)
-        mapValues.splice(pathValuesLength + index, 1)
-      }
-    } else {
-      switch (type) {
-        case 'Brands': {
-          mapValues.push('b')
-          break
-        }
-        case 'SpecificationFilters': {
-          mapValues.push(`${getSpecificationFilterFromLink(link)}`)
-          break
-        }
-        default: {
-          mapValues.push('c')
-        }
-      }
-      restValues.push(name)
-    }
-  }
-
-  const queryString = QueryString.stringify({
-    map: mapValues.join(','),
-    page: pageNumber !== 1 ? pageNumber : undefined,
-    order: orderBy === SortOptions[0].value ? undefined : orderBy,
-    rest: restValues.join(',') || undefined,
-  })
-  const params = matchPagesPath(getPathOfPage(pagesPath), pathName)
-  return { page: pagesPath, params, queryString }
+  return new Parser(pagesJson.pages[pagesPath].path || '').reverse(params)
 }
