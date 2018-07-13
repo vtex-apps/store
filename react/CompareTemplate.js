9da20f4e185ba74aa343054b63362d5bbb3ac1a0
Guilherme Rodrigues <gadr90@gmail.com>
2018-07-13 16:02:02 -0300

    Create new template structure (#27)
    * Create new template structure
    
    * Remove prefetching
    
    * Change pages to be an array of configurations
    
    * Release v1.0.0

--- react/CompareTemplate.js
+++ react/CompareTemplate.js
@@ -1,16 +1,7 @@
-import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { ExtensionPoint } from 'render'
 
 export default class ComparePage extends Component {
-  static contextTypes = {
-    prefetchPage: PropTypes.func,
-  }
-
-  componentDidMount() {
-    this.context.prefetchPage('store/home')
-  }
-
   render() {
     return (
       <ExtensionPoint id="container" />
---
+++ react/HomeTemplate.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { ExtensionPoint } from 'render'
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <ExtensionPoint id="container" />
+    )
+  }
+}
