22bc6ba55a697adb129eb26679b7889d92c64e59
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-07-27 09:35:29 -0300

    Fix search result filters not working

--- react/ProductSearchContextProvider.js
+++ react/ProductSearchContextProvider.js
@@ -51,7 +51,7 @@ class ProductSearchContextProvider extends Component {
             <DataLayerApolloWrapper
               getData={this.getData}
               loading={
-                contextProps.state.loading || contextProps.searchQuery.loading
+                contextProps.loading || contextProps.searchQuery.loading
               }
             >
               {React.cloneElement(this.props.children, contextProps)}
