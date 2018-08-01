b41ca68c41d061f79cb6831dd4205c995ffae1c3
Felipe Sales <sales.felipeb@gmail.com>
2018-08-01 14:49:16 -0300

    Refact ProductSearchContextProvider, removing ReactContext, update Changelog

--- react/ProductSearchContextProvider.js
+++ react/ProductSearchContextProvider.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react'
-import { path, last, head } from 'ramda'
 
 import DataLayerApolloWrapper from './components/DataLayerApolloWrapper'
-import SearchQueryContainer from './components/SearchQueryContainer'
-import { searchContextPropTypes } from './constants/propTypes'
-import { SearchQueryContext } from './constants/searchContext'
+
+import { processSearchContextProps } from './helpers/searchHelpers'
+import searchQuery from './queries/searchQuery.gql'
+import { Query } from 'react-apollo'
+
+const DEFAULT_PAGE = 1
+const DEFAULT_MAX_ITEMS_PER_PAGE = 1
 
 class ProductSearchContextProvider extends Component {
-  static propTypes = searchContextPropTypes
-
-  stripCategory(category) {
-    return category.replace(/^\/|\/$/g, '')
+  state = {
+    variables: {
+      maxItemsPerPage: DEFAULT_MAX_ITEMS_PER_PAGE,
+    },
+    /* Will be loading by default. The container will wait until the real data arrives */
+    loading: true,
   }
 
-  getData = () => {
-    const { searchQuery } = this.props
-
+  getData = searchQuery => {
     if (!searchQuery) {
       return null
     }
 
     const { products } = searchQuery
 
-    console.log('get dataLayer')
+    const category = this.props.params.category
+
     return [
       {
         ecommerce: {
@@ -31,47 +35,76 @@ class ProductSearchContextProvider extends Component {
             name: product.productName,
             list: 'Search Results',
             brand: product.brand,
-            category: this.stripCategory(path(['categories', '0'], product)),
+            category: searchQuery.facets.CategoriesTrees[index]
+              ? searchQuery.facets.CategoriesTrees[index].Name
+              : category,
             position: index + 1,
           })),
         },
       },
       {
         accountName: global.__RUNTIME__.account,
-        pageCategory: this.props.params.category,
+        pageCategory: category,
         pageDepartment: this.props.params.department,
+        pageFacets: [],
+        pageTitle: document.title,
+        pageUrl: window.location.href,
       },
     ]
   }
 
+  handleContextVariables = variables => {
+    this.setState({
+      variables,
+      loading: false,
+    })
+  }
+
   render() {
-    const props = {
-      ...this.props,
-      // todo: this logic should be in SearchQueryContainer
-      ...(this.props.params.brand && {
-        query: {
-          ...this.props.query,
-          map: this.props.map || 'b',
-        },
-      }),
-    }
+    const props = processSearchContextProps(
+      this.props,
+      this.state,
+      DEFAULT_PAGE
+    )
+
+    const { params, map, rest, orderBy, from, to } = props
 
     return (
-      <SearchQueryContainer {...props}>
-        <SearchQueryContext.Consumer>
-          {contextProps => (
-            <DataLayerApolloWrapper
-              getData={this.getData}
-              loading={contextProps.loading || contextProps.searchQuery.loading}
-            >
-              {console.log(
-                contextProps.loading || contextProps.searchQuery.loading
-              )}
-              {React.cloneElement(this.props.children, contextProps)}
-            </DataLayerApolloWrapper>
-          )}
-        </SearchQueryContext.Consumer>
-      </SearchQueryContainer>
+      <Query
+        query={searchQuery}
+        variables={{
+          query: Object.values(params)
+            .filter(s => s.length > 0)
+            .join('/'),
+          map,
+          rest,
+          orderBy,
+          from,
+          to,
+        }}
+        notifyOnNetworkStatusChange
+      >
+        {searchQueryProps => (
+          <DataLayerApolloWrapper
+            getData={() =>
+              this.getData({
+                ...searchQueryProps.data.search,
+              })
+            }
+            loading={this.state.loading || searchQueryProps.loading}
+          >
+            {React.cloneElement(this.props.children, {
+              loading: this.state.loading,
+              setContextVariables: this.handleContextVariables,
+              ...props,
+              searchQuery: {
+                ...searchQueryProps,
+                ...searchQueryProps.data.search,
+              },
+            })}
+          </DataLayerApolloWrapper>
+        )}
+      </Query>
     )
   }
 }
