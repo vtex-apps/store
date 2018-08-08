437b11efbcf8d4d1853ca1484fb16b6859ffe2de
Bruno Dias <bruno.dias@vtex.com.br>
2018-08-08 20:52:10 -0300

    Change GTM warning message and Change gtm warning message and organize the project

--- react/components/withDataLayer.js
+++ react/components/withDataLayer.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from '../../../../Library/Caches/typescript/2.9/node_modules/@types/react'
+import PropTypes from '../../../../Library/Caches/typescript/2.9/node_modules/@types/prop-types'
 
 const { Consumer, Provider } = React.createContext({
   dataLayer: [],
