1d1331b5074c4b82f544fc11ebd84ac0b7a0b970
Estácio Pereira <estacio.pereira@gmail.com>
2018-10-31 16:45:21 -0300

    Change sw register URL to be relative

--- public/pwa/workers/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(event) {
-  console.log('sw event: ', event)
-  event.waitUntil(Promise.resolve(true))
-})
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate: ', e)
-})
