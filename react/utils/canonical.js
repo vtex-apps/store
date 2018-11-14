791c5754ddcf8da50a42ded4ced306adfaf3fa73
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-14 11:42:49 -0300

    Remove logs

--- react/utils/canonical.js
+++ react/utils/canonical.js
@@ -1,10 +1,7 @@
 import { keys } from 'ramda'
 
 const canonicalPathFromParams = (canonicalTemplate, params) => {
-  console.log(params)
   const regex = new RegExp(`:(${keys(params).join('|')})`, 'g')
-
-  console.log(regex)
 
   return canonicalTemplate.replace(regex, (_, p) => params[p])
 }
