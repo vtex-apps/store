6757a3c5a14d6a188a0e26b87981b7cf83c66832
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-12-03 12:17:30 -0300

    Split logic between product context and wrapper

--- react/ProductWrapper.js
+++ react/ProductWrapper.js
@@ -1,78 +1,25 @@
 import PropTypes from 'prop-types'
 import React, { Component, Fragment } from 'react'
-import { withApollo, graphql, compose } from 'react-apollo'
-import { path, last, head, isEmpty } from 'ramda'
+import { last, head } from 'ramda'
 import { Helmet, withRuntimeContext } from 'render'
 
 import MicroData from './components/MicroData'
 import DataLayerApolloWrapper from './components/DataLayerApolloWrapper'
-import productQuery from './queries/productQuery.gql'
-import recommendationsAndBenefits from './queries/recommendationsAndBenefitsQuery.gql'
-import productPreviewFragment from './queries/productPreview.gql'
-import { cacheLocator } from './cacheLocator'
 
 class ProductWrapper extends Component {
   static propTypes = {
     params: PropTypes.object,
-    query: PropTypes.shape({
-      skuId: PropTypes.string,
-    }),
-    data: PropTypes.object,
+    productQuery: PropTypes.object,
     children: PropTypes.node,
     runtime: PropTypes.object,
   }
 
-  product() {
-    const {
-      catalog: {
-        product: catalogProduct,
-        loading: catalogLoading = true
-      } = {},
-      recommendationsAndBenefits: {
-        product: recAndBenefitsProduct,
-        loading: recAndBenefitsLoading = true
-      } = {}
-    } = this.props
-
-    const catalogInfo = !catalogLoading && catalogProduct
-    const recAndBenefitsInfo = catalogInfo && !recAndBenefitsLoading && recAndBenefitsProduct
-    const product = {
-      ...catalogInfo,
-      ...recAndBenefitsInfo
-    }
-    return isEmpty(product)
-      ? null
-      : product
-  }
-
-  loading() {
-    const {
-      catalog: {
-        loading: catalogLoading = true
-      } = {},
-      recommendationsAndBenefits: {
-        loading: recAndBenefitsLoading = true
-      } = {}
-    } = this.props
-
-    return catalogLoading || recAndBenefitsLoading
-  }
-
-  componentDidMount() {
-    this.checkNotFoundProduct();
-  }
-
-  componentDidUpdate() {
-    this.checkNotFoundProduct();
-  }
-
-  stripCategory(category) {
-    return category && category.replace(/^\/|\/$/g, '')
-  }
-
   getData = () => {
-    const { query, runtime: { account } } = this.props
-    const product = this.product()
+    const {
+      productQuery: { product },
+      runtime: { account },
+    } = this.props
+
     const {
       titleTag,
       brand,
@@ -114,7 +61,7 @@ class ProductWrapper extends Component {
       skuStockOutFromShelf: [],
     }
 
-    const skuId = query.skuId || (items && head(items).itemId)
+    const skuId = items && head(items).itemId
 
     const [sku] =
       (items && items.filter(product => product.itemId === skuId)) || []
@@ -149,57 +96,15 @@ class ProductWrapper extends Component {
     ]
   }
 
-  checkNotFoundProduct = () => {
-    const loading = this.loading()
-    const product = this.product()
-    const { params: { slug }, runtime } = this.props
-    if (!product && !loading) {
-      runtime.navigate({
-        page: 'store/search',
-        params: { term: slug },
-        query: `productLinkNotFound=${slug}`
-      })
-    }
-  }
-
   render() {
     const {
       params: { slug },
-      client,
+      productQuery,
+      productQuery: { product, loading },
+      ...props
     } = this.props
 
-    const productPreview = client.readFragment({
-      id: cacheLocator.product(slug),
-      fragment: productPreviewFragment,
-    })
-    const loadedProduct = this.product()
-    const product = loadedProduct
-      ? loadedProduct
-      : productPreview && productPreview.items
-        ? productPreview
-        : null
-
-    const loading = this.loading()
     const { titleTag, metaTagDescription } = product || {}
-
-    const productQuery = {
-      loading,
-      product,
-    }
-
-    if (!product && !loading) {
-      productQuery.error = {
-        message: 'Product not found!',
-      }
-    }
-
-    /**
-     * The breadcrumbs components is being used in multiple pages, therefore we need to adapt the data to its needs insteadof
-     * making the component do the changes it self.**/
-    const breadcrumbsProps = {
-      term: slug,
-      categories: product ? product.categories : null,
-    }
 
     return (
       <div className="vtex-product-context-provider">
@@ -209,47 +114,19 @@ class ProductWrapper extends Component {
             <meta name="description" content={metaTagDescription} />
           )}
         </Helmet>
-        {
-          <Fragment>
-            {product && <MicroData product={product} />}
-            <DataLayerApolloWrapper getData={this.getData} loading={loading}>
-              {React.cloneElement(this.props.children, {
-                productQuery,
-                slug,
-                ...breadcrumbsProps,
-              })}
-            </DataLayerApolloWrapper>
-          </Fragment>
-        }
+        <Fragment>
+          {product && <MicroData product={product} />}
+          <DataLayerApolloWrapper getData={this.getData} loading={loading}>
+            {React.cloneElement(this.props.children, {
+              productQuery,
+              slug,
+              ...props,
+            })}
+          </DataLayerApolloWrapper>
+        </Fragment>
       </div>
     )
   }
 }
 
-const catalogOptions = {
-  name: 'catalog',
-  options: props => ({
-    variables: {
-      slug: props.params.slug,
-    },
-    errorPolicy: 'all',
-  }),
-}
-
-const recommendationsAndBenefitsOptions = {
-  name: 'recommendationsAndBenefits',
-  options: props => ({
-    variables: {
-      slug: props.params.slug,
-    },
-    errorPolicy: 'all',
-    ssr: false,
-  }),
-}
-
-export default compose(
-  withApollo,
-  withRuntimeContext,
-  graphql(productQuery, catalogOptions),
-  graphql(recommendationsAndBenefits, recommendationsAndBenefitsOptions)
-)(ProductWrapper)
+export default withRuntimeContext(ProductWrapper)
