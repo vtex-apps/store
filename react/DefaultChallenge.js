5c4b0937abff8e7518106f408554498578609fe7
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-01-28 11:55:48 -0200

    Add default and profile challenge implementations

---
+++ react/DefaultChallenge.js
@@ -0,0 +1,5 @@
+const DefaultChallenge = ({ children }) => {
+  return children
+}
+
+export default DefaultChallenge
---
+++ react/ProfileChallenge.js
@@ -0,0 +1,81 @@
+import PropTypes from 'prop-types'
+import React, { PureComponent } from 'react'
+
+import { withRuntimeContext } from 'vtex.render-runtime'
+
+const LOGIN_PATH = '/login'
+const API_SESSION_URL = '/api/sessions?items=*'
+
+class ProfileChallenge extends PureComponent {
+  static propTypes = {
+    page: PropTypes.string,
+    pages: PropTypes.object,
+    runtime: PropTypes.shape({
+      navigate: PropTypes.func,
+    }),
+    children: PropTypes.node,
+    loader: PropTypes.element,
+  }
+
+  state = {
+    loading: true,
+    logged: false,
+  }
+
+  componentDidMount() {
+    this.onUpdate()
+  }
+
+  componentDidUpdate() {
+    this.onUpdate()
+  }
+
+  redirectToLogin() {
+    const pathName = window.location.pathname.replace(/\/$/, '')
+    if (this.props.page !== 'store.login' && pathName !== LOGIN_PATH) {
+      this.props.runtime.navigate({
+        fallbackToWindowLocation: false,
+        query: `returnUrl=${pathName}`,
+        to: LOGIN_PATH,
+      })
+    }
+  }
+
+  onUpdate() {
+    fetch(API_SESSION_URL, { credentials: 'same-origin' })
+      .then(response => response.json())
+      .then(response => {
+        if (
+          response.namespaces &&
+          (response.namespaces.authentication.storeUserId ||
+            response.namespaces.impersonate.storeUserId)
+        ) {
+          this.setState({ loading: false, logged: true })
+        } else {
+          this.setState({ loading: false, logged: false })
+          this.redirectToLogin()
+        }
+      })
+      .catch(() => {
+        this.setState({ loading: false, logged: false })
+        this.redirectToLogin()
+      })
+  }
+
+  render() {
+    const { children } = this.props
+    const { logged, loading } = this.state
+
+    if (loading) {
+      return <div className="flex justify-center ma4"></div>
+    }
+
+    if (logged) {
+      return children
+    }
+
+    return null
+  }
+}
+
+export default withRuntimeContext(ProfileChallenge)
