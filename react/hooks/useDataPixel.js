60be181e335954a0945be39150e11989d20523e3
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-05-09 17:46:58 -0300

    Add useDataPixel hook

---
+++ react/hooks/useDataPixel.js
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react'
+import { usePixel } from 'vtex.pixel-manager/PixelContext'
+
+const useDataPixel = (data, isLoading = false) => {
+  const { push } = usePixel()
+  const prevLoadingRef = useRef(true)
+
+  useEffect(() => {
+    if (prevLoadingRef.current && !isLoading) {
+      if (Array.isArray(data)) {
+        data.forEach(event => push(event))
+      } else {
+        push(data)
+      }
+    }
+
+    prevLoadingRef.current = isLoading
+  }, [data, isLoading, push])
+}
+
+export default useDataPixel
