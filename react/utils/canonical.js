46b91f7d16a15da147175dc44ca02be89dde23c6
Bruno Abreu <bruno.abreu@vtex.com.br>
2019-05-15 18:04:22 -0300

    Remove canonical discovery and  history replacement

--- react/utils/canonical.js
+++
@@ -1,9 +0,0 @@
-import { isEmpty } from 'ramda'
-import RouteParser from 'route-parser'
-
-const canonicalPathFromParams = (canonicalRouteTemplate, params) =>
-  !isEmpty(params) && canonicalRouteTemplate
-    ? new RouteParser(canonicalRouteTemplate).reverse(params)
-    : null
-
-export default canonicalPathFromParams
