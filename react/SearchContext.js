706acba890bc301203ca785c76e7aac7cc421884
Joao Fidelis <joaogfidelis@gmail.com>
2019-06-18 14:48:15 -0300

    share SearchQuery component with LocalQuery on search-result

--- react/SearchContext.js
+++ react/SearchContext.js
@@ -1,54 +1,13 @@
 import PropTypes from 'prop-types'
-import { path, zip, split, head, join, tail } from 'ramda'
-import React, { useMemo } from 'react'
-import { Query } from 'react-apollo'
+import { path } from 'ramda'
+import React from 'react'
 import { useRuntime } from 'vtex.render-runtime'
 
-import { productSearchV2 } from 'vtex.store-resources/Queries'
+import SearchQuery from 'vtex.search-result/SearchQuery'
+
 import { initializeMap, SORT_OPTIONS } from './utils/search'
 
-const DEFAULT_PAGE = 1
 const DEFAULT_MAX_ITEMS_PER_PAGE = 10
-
-const QUERY_SEPARATOR = '/'
-const MAP_SEPARATOR = ','
-
-const splitQuery = split(QUERY_SEPARATOR)
-const splitMap = split(MAP_SEPARATOR)
-const joinQuery = join(QUERY_SEPARATOR)
-const joinMap = join(MAP_SEPARATOR)
-
-const includeFacets = (map, query) =>
-  !!(map && map.length > 0 && query && query.length > 0)
-
-const useFacetsArgs = (query, map) => {
-  return useMemo(() => {
-    const queryArray = splitQuery(query)
-    const mapArray = splitMap(map)
-    const queryAndMap = zip(queryArray, mapArray)
-    const relevantArgs = [
-      head(queryAndMap),
-      ...tail(queryAndMap).filter(
-        ([_, tupleMap]) => tupleMap === 'c' || tupleMap === 'ft'
-      ),
-    ]
-    const { finalMap, finalQuery } = relevantArgs.reduce(
-      (accumulator, [tupleQuery, tupleMap]) => {
-        accumulator.finalQuery.push(tupleQuery)
-        accumulator.finalMap.push(tupleMap)
-        return accumulator
-      },
-      { finalQuery: [], finalMap: [] }
-    )
-    const facetQuery = joinQuery(finalQuery)
-    const facetMap = joinMap(finalMap)
-    return {
-      facetQuery,
-      facetMap,
-      withFacets: includeFacets(facetMap, facetQuery),
-    }
-  }, [map, query])
-}
 
 const SearchContext = ({
   nextTreePath,
@@ -70,51 +29,30 @@ const SearchContext = ({
 }) => {
   const { page: runtimePage } = useRuntime()
   const map = mapQuery || initializeMap(params)
-  const page = pageQuery ? parseInt(pageQuery) : DEFAULT_PAGE
-  const from = (page - 1) * maxItemsPerPage
-  const to = from + maxItemsPerPage - 1
 
   const query = Object.values(params)
     .filter(term => term && term.length > 0)
     .join('/')
     .replace(/\/\//g, '/') //This cleans some bad cases of two // on some terms.
 
-  const defaultSearch = {
-    query:
-      rest && rest.length > 0 ? `${query}/${rest.replace(',', '/')}` : query,
-    map,
-    orderBy,
-    priceRange,
-    from,
-    to,
-    rest,
-    hideUnavailableItems,
-  }
+  const queryValue = queryField
+    ? queryField
+    : rest && rest.length > 0
+    ? `${query}/${rest.replace(',', '/')}`
+    : query
+  const mapValue = queryField ? mapField : map
 
-  const customSearch = {
-    query: queryField,
-    map: mapField,
-    orderBy,
-    priceRange,
-    from,
-    to,
-    hideUnavailableItems,
-  }
-
-  const queryVariables = queryField ? customSearch : defaultSearch
-  const facetsArgs = useFacetsArgs(queryVariables.query, queryVariables.map)
-  const variablesWithFacets = {
-    ...queryVariables,
-    ...facetsArgs,
-  }
   return (
-    <Query
-      query={productSearchV2}
-      variables={variablesWithFacets}
-      notifyOnNetworkStatusChange
-      partialRefetch
+    <SearchQuery
+      maxItemsPerPage={maxItemsPerPage}
+      query={queryValue}
+      map={mapValue}
+      orderBy={orderBy}
+      priceRange={priceRange}
+      hideUnavailableItems={hideUnavailableItems}
+      pageQuery={pageQuery}
     >
-      {searchQuery => {
+      {(searchQuery, extraParams) => {
         return React.cloneElement(children, {
           searchQuery: {
             ...searchQuery,
@@ -138,15 +76,15 @@ const SearchContext = ({
           map,
           orderBy,
           priceRange,
-          page,
-          from,
-          to,
+          page: extraParams.page,
+          from: extraParams.from,
+          to: extraParams.to,
           maxItemsPerPage,
           // backwards-compatibility
           rest,
         })
       }}
-    </Query>
+    </SearchQuery>
   )
 }
 
