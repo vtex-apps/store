18898eb7af92332aed9ce14d2348fa9c9916a095
Joao Fidelis <joaogfidelis@gmail.com>
2019-07-01 09:55:56 -0300

    more test

--- react/__tests__/ProductWrapper.test.js
+++ react/__tests__/ProductWrapper.test.js
@@ -4,7 +4,6 @@ import { render, act } from '@vtex/test-tools/react'
 import { getProduct, getItem } from '../__mocks__/productMock'
 import ProductWrapper from '../ProductWrapper'
 import { ProductContext } from '../__mocks__/vtex.product-context'
-import { ProductDispatchContext } from '../__mocks__/vtex.product-context/ProductDispatchContext'
 
 const ProductPageMock = () => {
   const { selectedItem, product, selectedQuantity } = useContext(ProductContext)
@@ -44,10 +43,8 @@ describe('ProductWrapper component', () => {
 
   it('should render with correct title and no meta', () => {
     const product = getProduct()
-    const { queryByText, queryByTestId } = renderComponent()
-
-    expect(queryByText('<title>')).toBeDefined()
-    expect(queryByText(product.titleTag)).toBeDefined()
+    const { queryByTestId, getByText } = renderComponent()
+    getByText(new RegExp(product.titleTag))
     // Doesnt have metatag description, <meta> should not be rendered
     expect(queryByTestId('descritpion')).toBeNull()
   })
@@ -75,15 +72,11 @@ describe('ProductWrapper component', () => {
       titleTag: 'Product 2',
       productName: 'Product 2',
     })
-    const { dispatch } = useContext(ProductDispatchContext)
-    const quantity = 3
-    dispatch({ type: 'SET_QUANTITY', args: { quantity } })
 
     getByText(new RegExp(product.titleTag))
     getByText(`Selected Item id: ${product.items[0].itemId}`)
     getByText(`Selected Item name: ${product.items[0].name}`)
     getByText(`product slug: ${product.linkText}`)
-    getByText(`Selected Quantity: ${quantity}`)
     const newProps = {
       product: newProduct,
       params: { slug: newProduct.linkText },
@@ -101,17 +94,16 @@ describe('ProductWrapper component', () => {
     getByText(`Selected Item id: ${newProduct.items[0].itemId}`)
     getByText(`Selected Item name: ${newProduct.items[0].name}`)
     getByText(`product slug: ${newProduct.linkText}`)
-    getByText(`Selected Quantity: 1`)
   })
 
-  it('should select item with available quantity', async () => {
+  it('should select first item with available quantity', async () => {
     const noQuantity = getItem('no quantity', 90, 0)
     const itemWithQuantity = getItem('Item with quantity', 90, 10)
     const otherItemWithQuantity = getItem('other item with quantity', 90, 10)
     const newProduct = getProduct({
       items: [noQuantity, noQuantity, itemWithQuantity, otherItemWithQuantity],
     })
-    const { getByText, debug } = renderComponent({
+    const { getByText } = renderComponent({
       product: newProduct,
       params: { slug: newProduct.linkText },
       productQuery: { product: newProduct, loading: false },
@@ -119,5 +111,37 @@ describe('ProductWrapper component', () => {
 
     getByText(`Selected Item id: ${itemWithQuantity.itemId}`)
     getByText(`Selected Item name: ${itemWithQuantity.name}`)
+  })
+
+  it('should switch items when changing query prop', async () => {
+    const itemone = getItem('1', 90, 1)
+    const itemtwo = getItem('2', 90, 10)
+    const itemthree = getItem('3', 90, 10)
+    const newProduct = getProduct({
+      items: [itemone, itemtwo, itemthree],
+    })
+    const props = {
+      product: newProduct,
+      params: { slug: newProduct.linkText },
+      productQuery: { product: newProduct, loading: false },
+    }
+    const { getByText, rerender } = renderComponent(props)
+
+    getByText(`Selected Item id: ${itemone.itemId}`)
+    getByText(`Selected Item name: ${itemone.name}`)
+
+    const newProps = {
+      ...props,
+      query: { skuId: itemtwo.itemId },
+    }
+    rerender(
+      <ProductWrapper {...getProps(newProps)}>
+        <ProductPageMock />
+      </ProductWrapper>
+    )
+
+    act(() => {})
+    getByText(`Selected Item id: ${itemtwo.itemId}`)
+    getByText(`Selected Item name: ${itemtwo.name}`)
   })
 })
