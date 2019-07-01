da230bfd17d9424297c939a64cf3b6b12046c143
Lucas Bebber <lucasbbebber@gmail.com>
2019-07-01 10:58:24 -0300

    Add w-100 to WrapperContainer

--- react/components/WrapperContainer.js
+++ react/components/WrapperContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const WrapperContainer = ({ children, className }) => (
   <div className={className}>
-    <div className="flex flex-column min-vh-100">{children}</div>
+    <div className="flex flex-column min-vh-100 w-100">{children}</div>
   </div>
 )
 
