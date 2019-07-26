1849f3d76e851600f7b5ac1e648bf20fe95eefbc
Joao Fidelis <joaogfidelis@gmail.com>
2019-07-26 10:40:01 -0300

    fix names in default state

--- react/ProductWrapper.js
+++ react/ProductWrapper.js
@@ -26,7 +26,7 @@ const defaultState = {
   },
   assemblyOptions: {
     items: {},
-    areAssemblyGroupsValid: {},
+    areGroupsValid: {},
   },
 }
 
@@ -72,7 +72,7 @@ function reducer(state, action) {
           areGroupsValid: {
             ...state.assemblyOptions.areGroupsValid,
             [groupId]: isValid,
-          }
+          },
         },
       }
     }
