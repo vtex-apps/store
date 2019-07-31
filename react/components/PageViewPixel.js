31146cf27c13e1bacbc5233654545c73de8fed47
Joao Fidelis <joaogfidelis@gmail.com>
2019-07-31 14:46:05 -0300

    add explaining comment

--- react/components/PageViewPixel.js
+++ react/components/PageViewPixel.js
@@ -5,16 +5,21 @@ import { useRuntime } from 'vtex.render-runtime'
 // Page view events for home, search and product pages are sent by their own wrappers!
 const BLACKLIST_PAGES = ['store.home', 'store.search', 'store.product']
 
+/** When you navigate to a new page, the route object will remain the one from the previous route.
+ * So we save the ref on the first mount. After that, we wait for the first time route changes to send a page view with the correct title.
+ * A possible better implementation for the future, is add a loading state in the route object.
+ */
+
 const PageViewPixel = ({ title }) => {
   const { push } = usePixel()
   const { route, page, account } = useRuntime()
 
   const routeRef = useRef(route)
+  const isBlacklisted =
+    BLACKLIST_PAGES.findIndex(elem => page.includes(elem)) >= 0
 
-  if (
-    routeRef.current !== route &&
-    BLACKLIST_PAGES.findIndex(elem => page.includes(elem)) < 0
-  ) {
+  if (routeRef.current !== route && !isBlacklisted) {
+    routeRef.current = route
     const data = {
       event: 'pageView',
       pageTitle: title,
