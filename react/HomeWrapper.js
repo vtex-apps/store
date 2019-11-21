7763095bda20bfbb7e3bff43892b665cf99d4ed0
Breno Calazans <brenocalazans@gmail.com>
2019-11-21 18:55:40 -0300

    Change events of Search

--- react/HomeWrapper.js
+++ react/HomeWrapper.js
@@ -1,28 +1,18 @@
 import React, { useMemo, Fragment } from 'react'
 import PropTypes from 'prop-types'
-import { useRuntime } from 'vtex.render-runtime'
+import { useRuntime, canUseDOM } from 'vtex.render-runtime'
 import SearchAction from 'vtex.structured-data/SearchAction'
-
 import useDataPixel from './hooks/useDataPixel'
 
 const HomeWrapper = ({ children }) => {
-  const { account } = useRuntime()
+  const { account, route } = useRuntime()
 
   const pixelEvents = useMemo(() => {
-    if (typeof document === 'undefined') {
+    if (!canUseDOM) {
       return null
     }
+
     return [
-      {
-        event: 'pageView',
-        pageTitle: document.title,
-        pageUrl: location.href,
-        referrer:
-          document.referrer.indexOf(location.origin) === 0
-            ? undefined
-            : document.referrer,
-        accountName: account,
-      },
       {
         event: 'pageInfo',
         eventType: 'homeView',
@@ -33,7 +23,8 @@ const HomeWrapper = ({ children }) => {
       },
     ]
   }, [account])
-  useDataPixel(pixelEvents, 'Home')
+
+  useDataPixel(pixelEvents, route.routeId)
 
   return (
     <Fragment>
