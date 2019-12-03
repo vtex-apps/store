04e3f5d2192af4577818fa13dcc4616b6574df6c
Jeymisson Oliveira <jeymissoncz@gmail.com>
2019-12-03 10:45:13 -0300

    Add tests to navigation modifier

---
+++ react/__tests__/navigation.test.js
@@ -0,0 +1,31 @@
+import { normalizeNavigation } from '../utils/navigation'
+
+describe('Navigation route modifier', () => {
+  window['__RUNTIME__'] = {
+    route: {
+      domain: 'store',
+    },
+  }
+
+  it('should lowercase the path', () => {
+    const path = '/Category1/category2/Category3'
+    const query =
+      'map=c,c,c,specificationFilter_1&query=/Category1/category2/Category3/xpto'
+    const expectedPath = path.toLocaleLowerCase()
+    const expectedQuery = query
+    const result = normalizeNavigation({ path, query })
+
+    expect(result).toEqual({ path: expectedPath, query: expectedQuery })
+  })
+  it('should sort specification filters', () => {
+    const path = '/Category1/category2/Category3'
+    const query =
+      'map=c,c,c,specificationFilter_2,specificationFilter_0,specificationFilter_1&query=/Category1/category2/Category3/xpto2/xpto0/xpto1'
+    const expectedPath = path.toLocaleLowerCase()
+    const expectedQuery =
+      'map=c,c,c,specificationFilter_0,specificationFilter_1,specificationFilter_2&query=/Category1/category2/Category3/xpto0/xpto1/xpto2'
+    const result = normalizeNavigation({ path, query })
+
+    expect(result).toEqual({ path: expectedPath, query: expectedQuery })
+  })
+})
