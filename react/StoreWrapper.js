1f529ac197c084fcbb9d7baa1f99b163a8c28693
Arian Gallardo <gallardo.a@pucp.pe>
2021-04-01 10:24:49 -0500

    Fix: Changed sw register route
    Changed sw register route

--- react/StoreWrapper.js
+++ react/StoreWrapper.js
@@ -152,7 +152,7 @@ const StoreWrapper = ({ children, CustomContext }) => {
         script={[
           supportsServiceWorker && {
             type: 'text/javascript',
-            src: `${rootPath}/service-worker/register.js${queryMatch}${
+            src: `${rootPath}/register.js${queryMatch}${
               enableServiceWorker ? '' : '&__disableSW=true'
             }&scope=${encodeURIComponent(rootPath)}`,
             defer: true,
