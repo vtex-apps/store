6accebd57f69ca8b586c8e8012bcad0656784bff
Davi Guimarães <davi_guimaraes_ext@carrefour.com>
2023-04-11 14:59:18 -0300

    fix: ADD location.search  in url redirect on ProfileChallenge

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+- ADD `location.search` in url redirect on `ProfileChallenge`
 
 ## [2.131.0] - 2023-03-28
 
--- react/ProfileChallenge.tsx
+++ react/ProfileChallenge.tsx
@@ -14,13 +14,14 @@ const loginPath = '/login'
 const getLocation = () =>
   canUseDOM
     ? {
-        url: window.location.pathname + window.location.hash,
+        url: window.location.pathname + window.location.search + window.location.hash,
         pathName: window.location.pathname,
       }
     : {
         url: (global as any).__pathname__,
         pathName: (global as any).__pathname__,
       }
+
 
 const useSessionResponse = () => {
   const [session, setSession] = useState<SessionResponse>()
