e2e1390ef021f79c8e7f808a6f6e6aca4bf5a221
Rodrigo Orem <rodrigo.orem@vtex.com>
2025-08-18 13:47:12 -0300

    feat: wrap the store around AdsProvider

--- react/components/UserDataPixel.tsx
+++ react/components/UserDataPixel.tsx
@@ -1,71 +1,11 @@
 import { useEffect, FC } from 'react'
 import { usePixel } from 'vtex.pixel-manager/PixelContext'
 
-const fields = [
-  'firstName',
-  'lastName',
-  'document',
-  'id',
-  'email',
-  'phone',
-  'isAuthenticated',
-] as const
-
-interface SessionResponse {
-  response: {
-    namespaces: {
-      profile?: {
-        firstName?: {
-          value: string
-        }
-        lastName?: {
-          value: string
-        }
-        document?: {
-          value: string
-        }
-        id?: {
-          value: string
-        }
-        email?: {
-          value: string
-        }
-        phone?: {
-          value: string
-        }
-        isAuthenticated?: {
-          value: string
-        }
-      }
-    }
-  }
-}
-
-const getSessionPromiseFromWindow: any = () =>
-  !(window as any).__RENDER_8_SESSION__ ||
-  !(window as any).__RENDER_8_SESSION__.sessionPromise
-    ? Promise.resolve(null)
-    : (window as any).__RENDER_8_SESSION__.sessionPromise
-
-const toBoolean = (value: string) => value.toLowerCase() === 'true'
-
-function getUserData(
-  profileFields: SessionResponse['response']['namespaces']['profile']
-) {
-  if (!profileFields) {
-    return {}
-  }
-
-  return fields.reduce<Record<string, string | boolean>>((acc, key) => {
-    const value = profileFields[key]?.value
-
-    if (value) {
-      acc[key] = key === 'isAuthenticated' ? toBoolean(value) : value
-    }
-
-    return acc
-  }, {})
-}
+import {
+  getSessionPromiseFromWindow,
+  getUserData,
+  SessionResponse,
+} from '../hooks/getUserData'
 
 const UserDataPixel: FC = () => {
   const { push } = usePixel()
